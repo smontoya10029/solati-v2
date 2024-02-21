2a37b7b7d4c590e0bd2c1b2ce12f9ad2995e1c92
stiven montoya <desarrollador.cb@lge.com>
2024-02-21 14:26:33 -0500

    repositorio

---
+++ solati/postgres/app/Http/Controllers/solati/EmployesController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers\solati;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Employe;
+
+class EmployesController extends Controller
+{
+    
+    public function index()
+    {
+
+        try{
+
+            $employes = Employe::all();
+            return $employes;
+
+        }catch(\throwable $th) {
+
+            return response()->json([ 'erro' => $th->getMessage() ], 500);
+
+        }
+
+    }
+
+    public function add(Request $request)
+    {
+
+        try{
+
+            $data['nombre'] = $request['nombre'];
+            $data['apellido'] = $request['apellido'];
+            $data['telefono'] = $request['telefono'];
+            $res = Employe::create($data);
+            return response()->json($res, 200);
+
+        }catch(\throwable $th) {
+
+            return response()->json([ 'erro' => $th->getMessage() ], 500);
+
+        }
+
+    }
+
+    public function getById($id)
+    {
+
+        try{
+
+            $data = Employe::find($id);
+            return response()->json($data, 200);
+
+        }catch(\throwable $th) {
+
+            return response()->json([ 'erro' => $th->getMessage() ], 500);
+
+        }        
+
+    }
+
+    public function update(Request $request, $id)
+    {
+
+        try{
+
+            $employe = Employe::findOrFail($request->id);
+            $employe->nombre = $request->nombre;
+            $employe->apellido = $request->apellido;
+            $employe->telefono = $request->telefono;
+
+            $employe->save();
+
+            return $employe;
+
+        }catch(\throwable $th) {
+
+            return response()->json([ 'erro' => $th->getMessage() ], 500);
+
+        }
+
+    }
+
+    public function delete($id)
+    {
+
+        try{
+
+            $data = Employe::find($id)->delete();
+            return response()->json([ 'delete' => $data ], 200);
+
+        }catch(\throwable $th) {
+
+            return response()->json([ 'erro' => $th->getMessage() ], 500);
+
+        }        
+
+    }
+
+}
---
+++ solati/postgres/routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\solati\EmployesController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+//se crea prefijo de api rest de crud de empleados
+Route::prefix('v1/employes')->group( function () {
+    Route::get('/', [ EmployesController::class, 'index' ]); //mostras los empleados
+    Route::post('/', [ EmployesController::class, 'add' ] ); //agregar un nuevo empleado
+    Route::get('/{id}', [ EmployesController::class, 'getById' ] ); //mostrar un empleado en espesifico
+    Route::put('/{id}', [ EmployesController::class, 'update' ] ); //actualizar los datos de un empleado
+    Route::delete('/{id}', [ EmployesController::class, 'delete' ] ); //eliminar un empleado
+});
+
